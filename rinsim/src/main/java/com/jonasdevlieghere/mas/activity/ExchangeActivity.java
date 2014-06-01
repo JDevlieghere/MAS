877c557b0e6e78a560dd1e6c49818054ca012653
Dieter Castel <dietercastel@gmail.com>
2014-06-01 16:32:26 +0200

    Fixed Meeting case in Exchange.

--- rinsim/src/main/java/com/jonasdevlieghere/mas/activity/ExchangeActivity.java
+++ rinsim/src/main/java/com/jonasdevlieghere/mas/activity/ExchangeActivity.java
@@ -1,15 +1,17 @@
 package com.jonasdevlieghere.mas.activity;
 
+import com.google.common.collect.ImmutableSet;
 import com.jonasdevlieghere.mas.beacon.DeliveryTruck;
-import com.jonasdevlieghere.mas.communication.ExchangeReplyMessage;
-import com.jonasdevlieghere.mas.communication.ExchangeRequestMessage;
-import com.jonasdevlieghere.mas.communication.ExchangeStatus;
-import com.jonasdevlieghere.mas.communication.MessageStore;
+import com.jonasdevlieghere.mas.cluster.Cluster;
+import com.jonasdevlieghere.mas.cluster.KMeans;
+import com.jonasdevlieghere.mas.communication.*;
 import rinde.sim.core.TimeLapse;
 import rinde.sim.core.graph.Point;
 import rinde.sim.core.model.pdp.PDPModel;
+import rinde.sim.core.model.pdp.Parcel;
 import rinde.sim.core.model.road.RoadModel;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
@@ -35,23 +37,68 @@ public class ExchangeActivity extends Activity{
     @Override
     public void execute(RoadModel rm, PDPModel pm, TimeLapse time) {
         DeliveryTruck truck = (DeliveryTruck) getUser();
+        DeliveryTruck otherTruck;
         if(!truck.isPinged()){
             switch (status){
                 case INITIAL:
-                    DeliveryTruck otherTruck = getNearestTruck(rm, truck);
+                    otherTruck = getNearestTruck(rm, truck);
                     if(otherTruck != null){
                         //Ensure you are the first and only one to initiate exchange
                         if(otherTruck.ping()){
                             truck.send(otherTruck,new ExchangeRequestMessage(truck));
                         }
                     }
+                    status = ExchangeStatus.PENDING;
                     break;
                 case PENDING:
                     status = ExchangeStatus.MEETING;
                     setStatus(ActivityStatus.END_TICK);
                     break;
                 case MEETING:
-                    messageStore.retrieve(ExchangeReplyMessage.class);
+                    List<ExchangeReplyMessage> messages = messageStore.retrieve(ExchangeReplyMessage.class);
+                    //At all times there should only be one message of this type.
+                    ExchangeReplyMessage reply = messages.get(0);
+                    otherTruck = (DeliveryTruck) reply.getSender();
+                    ArrayList<Point> pointList = new ArrayList<Point>();
+                    ImmutableSet<Parcel> myParcels = pm.getContents(truck);
+                    ImmutableSet<Parcel> otherParcels = reply.getParcels();
+                    for(Parcel p: myParcels){
+                        pointList.add(p.getDestination());
+                    }
+
+                    for(Parcel p:otherParcels){
+                        pointList.add(p.getDestination());
+                    }
+                    KMeans km = new KMeans(pointList ,2);
+                    ArrayList<Cluster> clusters = km.getClusters();
+                    ArrayList<Point> myPoints = clusters.get(0).getPoints();
+                    ArrayList<Point> otherPoints = clusters.get(1).getPoints();
+
+                    ArrayList<Point> myPickupList = new ArrayList<Point>();
+                    ArrayList<Point> myDropList = new ArrayList<Point>();
+
+                    ArrayList<Point> otherPickupList = new ArrayList<Point>();
+                    ArrayList<Point> otherDropList = new ArrayList<Point>();
+
+                    for(Parcel parcel: myParcels){
+                        Point dest = parcel.getDestination();
+                        if(otherPoints.contains(dest)){
+                           otherDropList.add(dest);
+                           myPickupList.add(dest);
+                        }
+                    }
+
+                    for(Parcel parcel: otherParcels){
+                        Point dest = parcel.getDestination();
+                        if(myPoints.contains(dest)){
+                            myDropList.add(dest);
+                            otherPickupList.add(dest);
+                        }
+                    }
+
+                    truck.send(otherTruck, new ExchangeAssignmentMessage(truck, otherDropList,otherPickupList));
+
+                    status = ExchangeStatus.EXCHANGING;
                     break;
                 case EXCHANGING:
 
@@ -61,8 +108,9 @@ public class ExchangeActivity extends Activity{
 
         } else {
             List<ExchangeRequestMessage> messages = messageStore.retrieve(ExchangeRequestMessage.class);
+            //At all times there should only be one message of this type.
             ExchangeRequestMessage request = messages.get(0);
-
+            truck.send(request.getSender(), new ExchangeReplyMessage(truck, pm.getContents(truck)));
         }
     }
 
--- rinsim/src/main/java/com/jonasdevlieghere/mas/communication/ExchangeReplyMessage.java
+++ rinsim/src/main/java/com/jonasdevlieghere/mas/communication/ExchangeReplyMessage.java
@@ -1,12 +1,23 @@
 package com.jonasdevlieghere.mas.communication;
 
+import com.google.common.collect.ImmutableSet;
 import rinde.sim.core.model.communication.CommunicationUser;
 import rinde.sim.core.model.communication.Message;
+import rinde.sim.core.model.pdp.Parcel;
 
 public class ExchangeReplyMessage extends Message{
+    ImmutableSet<Parcel> parcels;
 
     public ExchangeReplyMessage(CommunicationUser sender ) {
         super(sender);
     }
 
+    public ExchangeReplyMessage(CommunicationUser sender, ImmutableSet<Parcel> contents) {
+        super(sender);
+        parcels = contents;
+    }
+
+    public ImmutableSet<Parcel> getParcels(){
+       return parcels;
+    }
 }
