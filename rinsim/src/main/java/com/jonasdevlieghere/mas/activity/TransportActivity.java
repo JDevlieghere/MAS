72e9f87a0b5e2289da1ba8c0fe864afae1578da4
JDevlieghere <info@jonasdevlieghere.com>
2014-06-03 15:54:15 +0200

    Added earliest delivery first strategy

--- rinsim/src/main/java/com/jonasdevlieghere/mas/activity/TransportActivity.java
+++ rinsim/src/main/java/com/jonasdevlieghere/mas/activity/TransportActivity.java
@@ -5,7 +5,7 @@ import com.jonasdevlieghere.mas.beacon.BeaconTruck;
 import com.jonasdevlieghere.mas.common.TickStatus;
 import com.jonasdevlieghere.mas.simulation.BeaconModel;
 import com.jonasdevlieghere.mas.common.Scheduler;
-import com.jonasdevlieghere.mas.strategy.delivery.SmartDeliveryStrategy;
+import com.jonasdevlieghere.mas.strategy.delivery.EarliestDeliveryStrategy;
 import rinde.sim.core.TimeLapse;
 import rinde.sim.core.model.pdp.PDPModel;
 import rinde.sim.core.model.road.RoadModel;
@@ -16,7 +16,7 @@ public class TransportActivity extends Activity {
 
     public TransportActivity(ActivityUser user) {
         super(user);
-        this.scheduler = new Scheduler((BeaconTruck)user, new SmartDeliveryStrategy());
+        this.scheduler = new Scheduler((BeaconTruck)user, new EarliestDeliveryStrategy());
     }
 
     @Override
