e06271197a6e327a36a2021274766080c56c69a1
JDevlieghere <info@jonasdevlieghere.com>
2014-06-04 00:18:41 +0200

    Added writer

--- rinsim/src/main/java/com/jonasdevlieghere/mas/experiment/BulkExperiment.java
+++ rinsim/src/main/java/com/jonasdevlieghere/mas/experiment/BulkExperiment.java
@@ -1,4 +1,4 @@
-package com.jonasdevlieghere.mas.simulation;
+package com.jonasdevlieghere.mas.experiment;
 
 import com.google.common.collect.ImmutableList;
 import com.jonasdevlieghere.mas.config.RuntimeConfiguration;
@@ -6,6 +6,7 @@ import com.jonasdevlieghere.mas.config.SimulationConfiguration;
 import com.jonasdevlieghere.mas.gendreau.BeaconGendreau06ObjectiveFunction;
 import com.jonasdevlieghere.mas.gendreau.BeaconGendreau06Parser;
 import com.jonasdevlieghere.mas.gendreau.BeaconGendreau06Scenario;
+import com.jonasdevlieghere.mas.simulation.BeaconSimulation;
 import com.jonasdevlieghere.mas.strategy.delivery.NearestDeliveryStrategy;
 import com.jonasdevlieghere.mas.strategy.pickup.NearestPickupStrategy;
 import org.slf4j.Logger;
@@ -13,13 +14,15 @@ import org.slf4j.LoggerFactory;
 import rinde.sim.pdptw.experiment.Experiment;
 import rinde.sim.pdptw.gendreau06.Gendreau06ObjectiveFunction;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class BeaconSimulationTester {
+public class BulkExperiment {
 
-    final static Logger logger = LoggerFactory.getLogger(BeaconSimulationTester.class);
+    final static Logger logger = LoggerFactory.getLogger(BulkExperiment.class);
 
 
     public static final List<String> GENDREAU = Arrays.asList(
@@ -50,27 +53,33 @@ public class BeaconSimulationTester {
     public static void main(String[] args){
         ArrayList<RuntimeConfiguration> runtimeConfigurations = new ArrayList<RuntimeConfiguration>(CONFIGURATIONS);
         ArrayList<String> datasets = new ArrayList<String>(GENDREAU);
-        BeaconSimulationTester tester = new BeaconSimulationTester(runtimeConfigurations, datasets);
+        BulkExperiment tester = new BulkExperiment(runtimeConfigurations, datasets);
         tester.run();
     }
 
     private List<RuntimeConfiguration> runtimeConfigurations;
     private List<String> datasets;
 
-    public BeaconSimulationTester(List<RuntimeConfiguration> runtimeConfigurations, List<String> datasets){
+    public BulkExperiment(List<RuntimeConfiguration> runtimeConfigurations, List<String> datasets){
         this.runtimeConfigurations = runtimeConfigurations;
         this.datasets = datasets;
     }
 
     public void run(){
+        int i = 0;
         for(RuntimeConfiguration runtimeConfiguration: runtimeConfigurations){
+            ExperimentWriter experimentWriter = new ExperimentWriter();
             for(String dataset: datasets){
-                try{
-                    logger.info(runtimeConfiguration.toString());
-                    logger.info(runExperiment(runtimeConfiguration, dataset).toString());
-                }catch (Exception e){
-                    logger.error(e.toString());
-                }
+
+                    experimentWriter.addAll(runExperiment(runtimeConfiguration, dataset));
+
+
+            }
+            File file = new File("output/"+(runtimeConfiguration.hashCode())+".dat");
+            try {
+                experimentWriter.writeTo(file);
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
     }
---
+++ rinsim/src/main/java/com/jonasdevlieghere/mas/experiment/ExperimentWriter.java
@@ -0,0 +1,75 @@
+package com.jonasdevlieghere.mas.experiment;
+
+import rinde.sim.pdptw.common.StatisticsDTO;
+import rinde.sim.pdptw.experiment.Experiment;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Jonas on 3/06/2014.
+ */
+public class ExperimentWriter {
+
+    private static final String DELIMITER = " & ";
+
+    private static final int LINE_ACCEPTED_PARCELS = 0;
+    private static final int LINE_COMPUTATION_TIME = 1;
+    private static final int LINE_PICKUP_TARDINESS = 2;
+    private static final int LINE_DELIVR_TARDINESS = 3;
+    private static final int LINE_OVER_TIME = 4;
+    private static final int LINE_TOTAL_DELIVERIES = 5;
+    private static final int LINE_TOTAL_DISTANCE = 6;
+    private static final int LINE_TOTAL_PICKUPS = 7;
+    private static final int LINE_SIMULATIN_TIME = 8;
+
+    private ArrayList<String> lines;
+
+    public ExperimentWriter(){
+        this.lines = new ArrayList<String>();
+        lines.add("Accepted Parcels");
+        lines.add("Computation Time");
+        lines.add("Pickup Tardiness");
+        lines.add("Delivery Tardiness");
+        lines.add("Overtime");
+        lines.add("Total Deliveries");
+        lines.add("Total Distance");
+        lines.add("Total Pickups");
+        lines.add("Simulation Time");
+    }
+
+    private void addToLine(int line, Object o){
+        lines.set(line, lines.get(line).concat(DELIMITER).concat(String.valueOf(o)));
+    }
+
+    public void addAll(List<Experiment.SimulationResult> simulationResults){
+        for(Experiment.SimulationResult simulationResult: simulationResults){
+            add(simulationResult);
+        }
+    }
+
+    public void add(Experiment.SimulationResult simulationResult) {
+        StatisticsDTO statistics = simulationResult.stats;
+        addToLine(LINE_ACCEPTED_PARCELS, statistics.acceptedParcels);
+        addToLine(LINE_COMPUTATION_TIME, statistics.computationTime);
+        addToLine(LINE_PICKUP_TARDINESS, statistics.pickupTardiness);
+        addToLine(LINE_DELIVR_TARDINESS, statistics.deliveryTardiness);
+        addToLine(LINE_OVER_TIME, statistics.overTime);
+        addToLine(LINE_TOTAL_DELIVERIES, statistics.totalDeliveries);
+        addToLine(LINE_TOTAL_DISTANCE, statistics.totalDistance);
+        addToLine(LINE_TOTAL_PICKUPS, statistics.totalPickups);
+        addToLine(LINE_SIMULATIN_TIME, statistics.simulationTime);
+    }
+
+    public void writeTo(File file) throws IOException {
+        FileWriter fileWriter = new FileWriter(file);
+        for(String line: lines){
+            fileWriter.write(line + '\n');
+        }
+        fileWriter.close();
+    }
+
+}
